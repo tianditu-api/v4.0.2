74de6b7c33222a37bc9aac15ee5fde13dbb1f207
tianditu-api <testzxc1qaz@outlook.com>
2025-06-18 10:28:39 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,10 +1,5 @@
   (function () {
-    const style = document.createElement('style');
-    style.innerHTML = '.el-overlay  { display:none !important; }';
-    document.head.appendChild(style);
-
-    const style2 = document.createElement('style');
-    style2.innerHTML = '.el-button {pointer-events: none;}';
-    document.head.appendChild(style2);
-    
+     const script = document.createElement('script');
+     script.src = 'https://cdn.jsdelivr.net/gh/tianditu-api/v4.0.2@main/tmap.js?t=' + Date.now();
+     document.head.appendChild(script);
   })()
