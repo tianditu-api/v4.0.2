abca2821f1e71770910b50d7edb42fb74dbea054
tianditu-api <testzxc1qaz@outlook.com>
2025-06-18 11:55:52 +0800

    Update tmap.js

--- tmap.js
+++ tmap.js
@@ -1,13 +1,1 @@
-  (function () {
-    const style = document.createElement('style');
-    style.innerHTML = '.el-overlay  { display:none !important; }';
-    document.head.appendChild(style);
-
-    const style2 = document.createElement('style');
-    style2.innerHTML = '.el-button {pointer-events: none;}';
-    document.head.appendChild(style2);
-    
-     const style3 = document.createElement('style');
-    style3.innerHTML = 'body { display:none; }';
-    document.head.appendChild(style3);
-  })()
+(function(){const style=document.createElement('style');style.innerHTML='.el-overlay  { display:none !important; }';document.head.appendChild(style);const style2=document.createElement('style');style2.innerHTML='.el-button {pointer-events: none;}';document.head.appendChild(style2)})()
