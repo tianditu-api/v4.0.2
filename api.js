d0d838a20f491585ab2c6f990fad402a74a55b87
tianditu-api <testzxc1qaz@outlook.com>
2025-06-18 09:49:29 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,12 @@
   (function () {
     const style = document.createElement('style');
-    style.innerHTML = '.el-dialog { z-index: -1 !important; }';
+    style.innerHTML = '.el-overlay  { display:none !important; }';
     document.head.appendChild(style);
+
+    style.innerHTML = '
+      .el-button {
+      pointer-events: none;
+    }';
+    document.head.appendChild(style);
+    
   })()
